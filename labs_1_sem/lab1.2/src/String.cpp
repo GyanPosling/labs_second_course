01cf29832d8d95334b72c6730bb276bb50de6821
GyanPosling <okicok578@gmail.com>
2025-09-29 05:12:01 +0300

    First beginig version of impl String class

--- labs_1_sem/lab1.2/src/String.cpp
+++ labs_1_sem/lab1.2/src/String.cpp
@@ -10,30 +10,131 @@ String& String::operator=(const String& other) {
     return *this;
 }
 
-String String:: operator+(const String& other){
-    String resultString = new String(this->length + other.length); 
+String String::operator+(const String& other) {
+    String resultString(this->length + other.length);
     strcpy(resultString.data, this->data);
-    strcat(resultString.data, other.data)
+    strcat(resultString.data, other.data);
     return resultString;
 }
 
-String String:: operator+(const char* string){
-    String resultString = new String(strlen(string) + this->length);
+String String::operator+(const char* string) {
+    String resultString(strlen(string) + this->length);
     strcpy(resultString.data, this->data);
     strcat(resultString.data, string);
     return resultString;
 }
 
-String String:: operator+(const char* string, const String& object){
-    String resultString = new String(strlen(string) + object.length);
+String operator+(const char* string, const String& object) {
+    String resultString(strlen(string) + object.length);
     strcpy(resultString.data, string);
     strcat(resultString.data, object.data);
     return resultString;
 }
 
-String String:: operator+(const char* string1, const char& string2){
-    String resultString = new String(strlen(string1) + strlen(string2));
-    strcpy(resultString.data, string1);
-    strcat(resultString.data, string2);
-    return resultString;
+String& String::operator+=(const String& other) {
+    char* temp = new char[this->length + other.length + 1];
+    strcpy(temp, this->data);
+    strcat(temp, other.data);
+    delete[] this->data;
+    this->data = temp;
+    this->length += other.length;
+    return *this;
+}
+
+char& String::operator[](int index) {
+    return this->data[index];
+}
+
+const char& String::operator[](int index) const {
+    return this->data[index];
+}
+
+String String::operator()(int start, int end) const {
+    String result(end - start + 1);
+    for (int i = 0; i < end - start; i++) {
+        result.data[i] = this->data[start + i];
+    }
+    return result;
+}
+
+bool String::operator>(const String& other) const {
+    return this->length > other.length;
+}
+
+bool String::operator<(const String& other) const {
+    return this->length < other.length;
+}
+
+String& String::operator++() {
+    for (int i = 0; i < this->length; i++) {
+        if (this->data[i] != '\0') {
+            this->data[i] = this->data[i] + 1;
+        }
+    }
+    return *this;
+}
+
+String String::operator++(int) {
+    String temp(*this);
+    ++(*this);
+    return temp;
+}
+
+String& String::operator--() {
+    for (int i = 0; i < this->length; i++) {
+        if (this->data[i] != '\0') {
+            this->data[i] = this->data[i] - 1;
+        }
+    }
+    return *this;
+}
+
+String String::operator--(int) {
+    String temp(*this);
+    --(*this);
+    return temp;
+}
+
+std::ostream& operator<<(std::ostream& os, const String& string) {
+    if (string.data != nullptr) {
+        os << "'" << string.data << "'";
+    }
+    return os;
+}
+
+std::istream& operator>>(std::istream& is, String& string) {
+    char ch;
+    int capacity = 10;
+    int size = 0;
+    char* buffer = new char[capacity];
+    
+    while (is.get(ch) && ch != '\n') {
+        if (size >= capacity - 1) {
+            capacity *= 2;
+            char* newBuffer = new char[capacity];
+            strcpy(newBuffer, buffer);
+            delete[] buffer;
+            buffer = newBuffer;
+        }
+        buffer[size++] = ch;
+    }
+    buffer[size] = '\0';
+    
+    delete[] string.data;
+    string.length = size;
+    string.data = new char[string.length + 1];
+    strcpy(string.data, buffer);
+    
+    delete[] buffer;
+    return is;
+}
+
+String& String::operator=(const String& other) {
+    if (this != &other) {
+        delete[] this->data;
+        this->length = other.length;
+        this->data = new char[this->length + 1];
+        strcpy(this->data, other.data);
+    }
+    return *this;
 }
--- labs_1_sem/lab1.2/src/main.cpp
+++ labs_1_sem/lab1.2/src/main.cpp
@@ -1,5 +1,71 @@
-#include<iostream>
+#include "String.h"
+#include <iostream>
 
-int main(){
-    std::cout<<"Hello world";
+int main() {
+    std::cout << "Testing default constructor. Default s1: " << s1 << std::endl;
+    String s1;
+
+    std::cout << "Testing constructor with char*. Enter a string for s2: ";
+    std::cin >> s2;
+    String s2;
+    std::cout << "s2: " << s2 << std::endl;
+
+    std::cout << "Testing constructor with size. Enter size for s3: ";
+    int size;
+    std::cin >> size;
+    String s3(size);
+    std::cout << "s3 (size " << size << "): " << s3 << std::endl;
+
+    std::cout << "Testing copy constructor. s4 is a copy of s2. s4: " << s4 << std::endl;
+    String s4(s2);
+
+    std::cout << "Testing operator+. s5 = s2 + s4. Result: " << s5 << std::endl;
+    String s5 = s2 + s4;
+
+    std::cout << "Testing operator+. s6 = s2 + ' World'. Result: " << s6 << std::endl;
+    String s6 = s2 + " World";
+
+    std::cout << "Testing operator+. s7 = 'Test' + s2. Result: " << s7 << std::endl;
+    String s7 = "Test" + s2;
+
+    std::cout << "Testing operator+=. s2 += s4. New s2: " << s2 << std::endl;
+    s2 += s4;
+
+    std::cout << "Testing operator[]. Enter index for s2: ";
+    int index;
+    std::cin >> index;
+    std::cout << "s2[" << index << "]: " << s2[index] << std::endl;
+
+    std::cout << "Testing operator(). Enter start and end indices for s2 substring: ";
+    int start, end;
+    std::cin >> start >> end;
+    String s8 = s2(start, end);
+    std::cout << "s2(" << start << "," << end << "): " << s8 << std::endl;
+
+    std::cout << "Testing operator> and <. Enter string for s9: ";
+    std::cin >> s9;
+    String s9;
+    std::cout << "Enter string for s10: ";
+    std::cin >> s10;
+    String s10;
+    std::cout << "s9 > s10: " << (s9 > s10) << std::endl;
+    std::cout << "s9 < s10: " << (s9 < s10) << std::endl;
+
+    std::cout << "Testing operator++ and --. Enter string for s11: ";
+    std::cin >> s11;
+    String s11;
+    ++s11;
+    std::cout << "++s11 (encode): " << s11 << std::endl;
+    --s11;
+    std::cout << "--s11 (decode): " << s11 << std::endl;
+
+    std::cout << "Testing operator>>. Enter a string for s12: ";
+    String s12;
+    std::cin >> s12;
+    std::cout << "s12: " << s12 << std::endl;
+
+    std::cout << "Testing operator=. s13 = s2. Result: " << s13 << std::endl;
+    String s13 = s2;
+
+    return 0;
 }
