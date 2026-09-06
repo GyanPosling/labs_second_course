7121aa6d6b2cd3bd480d4a0f7874bf12ab377e90
GyanPosling <okicok578@gmail.com>
2026-09-06 13:46:29 +0300

    Keep only C labs

--- JavaLabs/src/main/java/com/bsuir/taskmanager/repository/ProjectRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bsuir.taskmanager.repository;
-
-import com.bsuir.taskmanager.model.entity.Project;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProjectRepository extends JpaRepository<Project, Long> {
-}
--- OSISP/lab04/src/consumer.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "queue.h"
-#include "stack.h"
-
-void createConsumer(Stack* consumers, int* semId);
-void deleteConsumer(Stack* consumers);
-
-void consumeMessage(Message *msg);
