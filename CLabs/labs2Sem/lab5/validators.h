09b9c6a58d18bdcdc0af79b58e962128ee458edc
GyanPosling <okicok578@gmail.com>
2026-09-06 13:48:00 +0300

    Keep only C++ labs

--- CLabs/labs2Sem/lab5/validators.h
+++
@@ -1,8 +0,0 @@
-#ifndef VALIDATORS_H
-#define VALIDATORS_H
-
-char* initString();
-int getValidInput(int min, int max);
-double getValidDoubleInput(double min, double max);
-
-#endif
--- JavaLabs/src/main/java/com/bsuir/taskmanager/exception/AsyncBulkTaskOperationNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.bsuir.taskmanager.exception;
-
-public class AsyncBulkTaskOperationNotFoundException extends RuntimeException {
-    public AsyncBulkTaskOperationNotFoundException(String message) {
-        super(message);
-    }
-}
